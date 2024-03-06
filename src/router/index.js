8a91aafa6ef95ac0f5e1731421dcf0a69f8c7f41
SlovenskyUjo <adrianondik7@gmail.com>
2024-03-06 21:11:18 +0100

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/home",
+      component: Home
+    },
+    {
+      path: "/about",
+      component: () => import('../views/About.vue')
+    },
+    {
+      path: "/team",
+      component: () => import('../views/Team.vue')
+    }
+  ]
+})
+
+export default router;
